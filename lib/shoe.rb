d15aefb6bb30904685b4b2137a8a5cac859519e9
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-25 01:01:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :author, :page_count, :genre
   attr_reader :title
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   def turn_page
